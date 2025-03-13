a612ed070017f41a73f9bf4a3bc7917716bfb65d
David Júnior <davidjunior051204@gmail.com>
2025-03-13 00:42:22 -0300

    feat: adiciona axios e configura requisição de login
    problema aparentemente com cors pra resolver

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,27 +1,22 @@
 import { v4 as uuid } from "uuid";
+import { api } from "./api";
 
 type SignInRequestData = {
     email: string;
     password: string;
 }
 
-const delay = (amount = 750) => new Promise(resolve => setTimeout(resolve,amount))
-
-export async function signInRequest(data: SignInRequestData ){
-    await delay()
-
-    return {
-        token: uuid(),
-        user: {
-            name: 'David Inacio',
-            email: 'davidjunior051204@gmail.com',
-            avatar_url: 'https://github.com/GitDavidJr.png'
-        }
-    }
+type User = {
+    name: string;
+    email: string;
+    avatar_url: string;
 }
 
-export async function recoverUserInformation() {
-    await delay()
+export async function signInRequest(data: SignInRequestData ){
+    
+    return await api.post('/login', data) // aparentemente o cors pode esta dando erro, pois consigo fazer requisições http pelo thunder client ou insomnia, mas não consigo fazer pelo front-end
+}
+export async function recoverUserInformation() { // passa o token
     return {
         user: {
             name: 'David Inacio',
